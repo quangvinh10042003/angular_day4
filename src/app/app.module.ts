aae921ac1dfaea3f042fa24938d48a4538e66a7b
Quang Vinh <qvinhdx@gmail.com>
2022-05-30 13:47:28 +0700

    cc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    DetailsComponent,
+    ErrorComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
